fb4fea2deb213345d96e19dbab197786fcd0228e
Emrullah Dagkusu <56932641+emrdagkusu@users.noreply.github.com>
2022-02-17 17:35:22 +0300

    first commit to public

---
+++ component---src-pages-404-js-6398ab7807dbbc125400.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{429:function(e,t,l){l.r(t);var n=l(7294);t.default=function(){return n.createElement("div",null,n.createElement("h1",null,"404"),n.createElement("p",null,"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6398ab7807dbbc125400.js.map
